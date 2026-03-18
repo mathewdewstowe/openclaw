ad665d9e69d64d195e70ba04b220aee931c0b029
Matthew Dewstowe <matthew@umified.com>
2026-03-18 17:44:20 +0000

    Add Nth Layer 11ty site scaffold

---
+++ nthlayer-11ty/eleventy.config.js
@@ -0,0 +1,11 @@
+export default function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "_site"
+    }
+  };
+}
