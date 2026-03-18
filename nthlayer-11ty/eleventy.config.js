e89839d1d049d4d2b4a8397d8b1a1922077a0c05
Matthew Dewstowe <matthew@umified.com>
2026-03-18 18:41:25 +0000

    Mirror Nth Layer live site into 11ty

--- nthlayer-11ty/eleventy.config.js
+++ nthlayer-11ty/eleventy.config.js
@@ -1,10 +1,12 @@
 export default function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "mirror/nthlayer.co.uk/wp-content": "wp-content" });
+  eleventyConfig.addPassthroughCopy({ "mirror/nthlayer.co.uk/wp-includes": "wp-includes" });
+  eleventyConfig.addPassthroughCopy({ "mirror/nthlayer.co.uk/wp-json": "wp-json" });
+  eleventyConfig.addPassthroughCopy({ "mirror/nthlayer.co.uk/robots.txt": "robots.txt" });
 
   return {
     dir: {
-      input: "src",
-      includes: "_includes",
+      input: "mirror/nthlayer.co.uk",
       output: "_site"
     }
   };
